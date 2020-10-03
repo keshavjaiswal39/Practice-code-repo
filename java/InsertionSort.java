424403ae762bbe3a91c6a65d4e412358b05fc9a6
Sahil Chowdhury <sahilc0708@gmail.com>
2020-10-03 19:03:31 +0530

    Modified Code

--- java/InsertionSort.java
+++ java/InsertionSort.java
@@ -1,22 +1,27 @@
 import java.util.*;
-
 public class InsertionSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter no. of Elements: ");
         int n = sc.nextInt();
         int a[] = new int[n];
+        System.out.println("Enter Array Elements: ");
         for(int i=0; i<n; i++){
             a[i] = sc.nextInt();
         }
-        System.out.print(insertionSort(a,n));
+        int result[] = insertionSort(a, n);
+        System.out.println("Sorted Array: ");
+        for(int val: result){
+            System.out.println(val);
+        }
+        
         sc.close();
     }
     static int[] insertionSort(int a[], int n){
-        for(int i=1; i<=n-1; i++){
-            int j=i-1; 
+        for(int i=1; i<n; i++){
+            int j = i-1;
             int val = a[i];
-            while(j>0 && a[j]>val){
+            while(j>=0 && a[j]>val){
                 a[j+1] = a[j];
                 j--;
             }
